7bc92a33b5661026e107dd0abc269af4094ff50f
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-07 17:29:01 -0500

    Sent a file to the client

--- calculator.js
+++ calculator.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 
 // Create root route
-app.get("/", function (req, res) { 
-  res.send("<h1>Hello World</h1>");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 // listening on port 3000
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
