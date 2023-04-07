0a322865ea3695a5dabb0d2aaa13880e01a838bb
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-07 17:46:02 -0500

    Installed body-parser

--- calculator.js
+++ calculator.js
@@ -5,6 +5,11 @@ const app = express();
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res){
+  res.send("Thanks for posting!")
+
+});
 // listening on port 3000
 app.listen(3000, function () {
   console.log("Server started on port 3000");
