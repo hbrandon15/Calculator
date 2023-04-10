ceaa6c180612b438e2c03fc5ec2bc5d847ac02d2
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2023-04-09 20:21:37 -0500

    added comment

--- calculator.js
+++ calculator.js
@@ -11,7 +11,8 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
-  var num1 = Number(req.body.num1);
+  // Need to convert to a Number since originally it is text
+  var num1 = Number(req.body.num1); 
   var num2 = Number(req.body.num2);
 
   var result = num1 + num2;
